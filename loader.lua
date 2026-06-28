9a4408e76f06139d98fc4de8a5c6827ed7fc050f
cool83birdcarfly02six <116028776+cool83birdcarfly02six@users.noreply.github.com>
2026-06-28 13:53:14 +0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local GameId = game.GameId
 
 local Links = {
     [1490130369] = {
-        ScriptLink = "https://api.luarmor.net/files/v3/loaders/b51325b35c4c7cf0eddf46b4f9657f74.lua",
+        ScriptLink = "https://raw.githubusercontent.com/cool83birdcarfly02six/A/refs/heads/main/a.lua",
         KeyLink = "https://discord.gg/3mrasRydM2"
     },
 }
